e7bb931adddfafa85a1d21e760e1eec14783be08
Andrey Grachev <andreysgra@gmail.com>
2026-07-25 16:14:41 +0300

    Создаёт представления со статическим контентом.

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,10 +1,14 @@
 import HeroView from '../view/hero-view';
 import {render} from '../framework/render';
+import MissionView from '../view/mission-view';
+import AdvantagesView from '../view/advantages-view';
 
 export default class MainPresenter {
   #container = null;
 
   #heroComponent = new HeroView();
+  #missionComponent = new MissionView();
+  #advantagesComponent = new AdvantagesView();
 
   constructor({container}) {
     this.#container = container;
@@ -14,11 +18,21 @@ export default class MainPresenter {
     this.#renderBoard();
   }
 
+  #renderAdvantages() {
+    render(this.#advantagesComponent, this.#container);
+  }
+
   #renderBoard() {
     this.#renderHero();
+    this.#renderMission();
+    this.#renderAdvantages();
   }
 
   #renderHero() {
     render(this.#heroComponent, this.#container);
+  }
+
+  #renderMission() {
+    render(this.#missionComponent, this.#container);
   }
 }
